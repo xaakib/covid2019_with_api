158d54c05a7a5e3a1d79ff4e742e03bb0eaf69cb
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-03 14:53:50 +0600

    api_services bug

--- lib/app/services/api_services.dart
+++ lib/app/services/api_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:covid2019_with_api/app/services/api.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class APIServices {
@@ -24,4 +25,44 @@ class APIServices {
         'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
     throw response;
   }
+
+  Future<int> getEnopointData({
+    @required String accessToken,
+    @required Endpoint endpoint,
+  }) async {
+    final uri = api.endpointUri(endpoint);
+    final response = await http.get(
+      uri.toString(),
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+     if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String>, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKey[endpoint];
+        final int result = endpointData[responseJsonKey];
+        if (result !=null) {
+          return result;
+          
+        }
+        
+      }
+    }
+    throw response;
+  }
+
+  static Map<Endpoint, String>_responseJsonKey ={
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'data',
+    Endpoint.caseConfirmed : 'data',
+    Endpoint.deaths:'data',
+    Endpoint.recovered: 'data',
+
+  };
+
+
+
+
+
+
 }
